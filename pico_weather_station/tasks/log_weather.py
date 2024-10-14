71aaf6e9bdc4e475a8f7c7a510a7df340c368bba
zNitche <53448802+zNitche@users.noreply.github.com>
2024-10-14 22:12:10 +0200

    weather logger integration

--- pico_weather_station/tasks/log_weather.py
+++ pico_weather_station/tasks/log_weather.py
@@ -1,12 +1,21 @@
-from lightberry.tasks.aio import ATaskBase
+from lightberry import ATaskBase
+from pico_weather_station.loggers import WeatherSensorsLogger
+from pico_weather_station import consts
 
 
 class LogWeather(ATaskBase):
     def __init__(self, config: dict[str, any]):
         debug_enabled = config.get("DEBUG")
-        super().__init__(periodic_interval=60, init_delay=60, logging=debug_enabled)
+        super().__init__(periodic_interval=30, init_delay=30, logging=debug_enabled)
 
         self.weather_logs_per_hour = config.get('WEATHER_LOGS_PER_HOUR')
 
+        self.__logger = WeatherSensorsLogger(logs_path=consts.WEATHER_LOGS_DIR_PATH,
+                                             logs_per_hour=self.weather_logs_per_hour)
+
     async def task(self):
-        pass
+        try:
+            self.__logger.log()
+
+        except Exception as e:
+            self._print_log(message="Error while logging weather", exception=e)
