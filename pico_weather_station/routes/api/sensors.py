ef5f08bc3ae4267dc838c6f8cf829d7f6b37b445
zNitche <53448802+zNitche@users.noreply.github.com>
2024-10-16 21:17:53 +0200

    added getting station vitals stats for core router

--- pico_weather_station/routes/api/sensors.py
+++ pico_weather_station/routes/api/sensors.py
@@ -11,14 +11,10 @@ sensors = Router("sensors", url_prefix="/api/sensors")
 
 @sensors.route("/")
 async def bulk(request: Request):
-    datetime = sensors_manager.get_datetime()
     temperature, pressure, humidity = sensors_manager.get_env_readings()
-    bat_voltage = sensors_manager.get_battery_voltage()
     internal_temp = sensors_manager.get_internal_temp()
 
     data = {
-        "datetime": datetime.to_iso_string(),
-        "bat_voltage": bat_voltage,
         "temp": temperature,
         "internal_temp": internal_temp,
         "pressure": pressure,
--- pico_weather_station/routes/core.py
+++ pico_weather_station/routes/core.py
@@ -1,12 +1,22 @@
-from lightberry import Router, Response, typing
-
-if typing.TYPE_CHECKING:
-    from lightberry import Request
+from lightberry import Router, Response
+from lightberry.shortcuts import jsonify
+from pico_weather_station import cache_db, sensors_manager
 
 
 core = Router("core")
 
 
-@core.route("/")
-async def home(request: Request):
-    return Response(payload="hello world")
+@core.route("/stats")
+async def bulk_stats(request):
+    datetime = sensors_manager.get_datetime()
+
+    weather_logger_data = cache_db.read("weather_logger")
+    last_weather_log = weather_logger_data.get("last_logged") if weather_logger_data else None
+
+    data = {
+        "datetime": datetime.to_iso_string() if datetime else None,
+        "battery_voltage": sensors_manager.get_battery_voltage(),
+        "last_weather_log": last_weather_log.to_iso_string() if last_weather_log else None
+    }
+
+    return Response(payload=jsonify(data))
