aee93cde41a26eccf96b0bfbdff3c6f5e1c7897d
zNitche <53448802+zNitche@users.noreply.github.com>
2024-10-23 22:48:53 +0200

    tweaked ToggleWlan task args typing + fixed getting wlan state for toggle

--- pico_weather_station/tasks/toggle_wlan.py
+++ pico_weather_station/tasks/toggle_wlan.py
@@ -3,27 +3,37 @@ from pico_weather_station import logger, devices_manager
 
 if typing.TYPE_CHECKING:
     from lightberry.modules import ServerHandlersManager
+    from lightberry.config import AppConfig
+    from typing import Type
 
 
 class ToggleWlan(ATaskBase):
-    def __init__(self, config: dict[str, any], server_handler: ServerHandlersManager):
+    def __init__(self, config: Type[AppConfig], server_handlers_manager: ServerHandlersManager):
         super().__init__(periodic_interval=300, init_delay=60, logging=config.get("DEBUG"))
 
         self.__wlan_schedule = config.get('WLAN_SCHEDULE')
-        self.__server_handler = server_handler
+        self.__server_handlers_manager = server_handlers_manager
+
+    def __get_next_wlan_state(self):
+        datetime = devices_manager.get_datetime()
+
+        disable_at = self.__wlan_schedule[0]
+        enable_at = self.__wlan_schedule[1]
+
+        if datetime.hour >= disable_at or datetime.hour <= enable_at:
+            return False
+
+        if enable_at <= datetime.hour <= disable_at:
+            return True
 
     async def task(self):
         try:
-            datetime = devices_manager.get_datetime()
+            wlan_state = self.__get_next_wlan_state()
+            wlan_state_changed = self.__server_handlers_manager.toggle_wlan(wlan_state)
 
-            disable_at = self.__wlan_schedule[0]
-            enable_at = self.__wlan_schedule[1]
-
-            wlan_state = False if disable_at <= datetime.hour <= enable_at else True
-            self.__server_handler.toggle_wlan(wlan_state)
-
-            self._print_log(message=f"WLAN state has been set to: {wlan_state}")
-            logger.info(message=f"WLAN state has been set to: {wlan_state}")
+            if wlan_state_changed:
+                self._print_log(message=f"WLAN state has been set to: {wlan_state}")
+                logger.info(message=f"WLAN state has been set to: {wlan_state}")
 
         except Exception as e:
             self._print_log(message="Error while toggling wlan", exception=e)
