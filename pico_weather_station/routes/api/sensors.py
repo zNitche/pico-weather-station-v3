9bf2e267a000b3b131ce173d243346de30b080e9
zNitche <53448802+zNitche@users.noreply.github.com>
2024-10-24 20:47:56 +0200

    added endpoint for getting current pv data

--- pico_weather_station/routes/api/sensors.py
+++ pico_weather_station/routes/api/sensors.py
@@ -1,16 +1,13 @@
-from lightberry import Router, Response, typing
+from lightberry import Router, Response
 from lightberry.shortcuts import jsonify
-from pico_weather_station import devices_manager
-
-if typing.TYPE_CHECKING:
-    from lightberry import Request
+from pico_weather_station import devices_manager, integrations_manager
 
 
 sensors = Router("sensors", url_prefix="/api/sensors")
 
 
 @sensors.route("/")
-async def bulk(request: Request):
+async def bulk(request):
     temperature, pressure, humidity = devices_manager.get_env_readings()
     internal_temp = devices_manager.get_internal_temp()
 
@@ -22,3 +19,23 @@ async def bulk(request: Request):
     }
 
     return Response(payload=jsonify(data))
+
+
+@sensors.route("/pv")
+async def pv_data(request):
+    data = await integrations_manager.get_pv_readings()
+
+    if not data:
+        return Response(status_code=204)
+
+    res = {
+        "battery_capacity": data[0].value,
+        "battery_voltage": data[1].value,
+        "battery_current": data[2].value,
+        "load_voltage": data[5].value,
+        "load_current": data[6].value,
+        "solar_voltage": data[9].value,
+        "solar_current": data[10].value,
+    }
+
+    return Response(payload=jsonify(res))
