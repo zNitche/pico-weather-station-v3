4d79a8db6b9f44047797e804cd756d05b204606c
zNitche <53448802+zNitche@users.noreply.github.com>
2024-10-24 20:45:58 +0200

    logging background tasks args typehints tweaks

--- pico_weather_station/tasks/log_vitals.py
+++ pico_weather_station/tasks/log_vitals.py
@@ -1,10 +1,14 @@
-from lightberry import ATaskBase
+from lightberry import ATaskBase, typing
 from pico_weather_station.data_loggers import VitalsDataLogger
 from pico_weather_station import logger
 
+if typing.TYPE_CHECKING:
+    from lightberry.config import AppConfig
+    from typing import Type
+
 
 class LogVitals(ATaskBase):
-    def __init__(self, config: dict[str, any]):
+    def __init__(self, config: Type[AppConfig]):
         super().__init__(periodic_interval=30, init_delay=30, logging=config.get("DEBUG"))
 
         self.__logs_per_hour = config.get("VITALS_LOGS_PER_HOUR")
--- pico_weather_station/tasks/log_weather.py
+++ pico_weather_station/tasks/log_weather.py
@@ -1,10 +1,14 @@
-from lightberry import ATaskBase
+from lightberry import ATaskBase, typing
 from pico_weather_station.data_loggers import WeatherDataLogger
 from pico_weather_station import logger
 
+if typing.TYPE_CHECKING:
+    from lightberry.config import AppConfig
+    from typing import Type
+
 
 class LogWeather(ATaskBase):
-    def __init__(self, config: dict[str, any]):
+    def __init__(self, config: Type[AppConfig]):
         super().__init__(periodic_interval=30, init_delay=30, logging=config.get("DEBUG"))
 
         self.__logs_per_hour = config.get('WEATHER_LOGS_PER_HOUR')
