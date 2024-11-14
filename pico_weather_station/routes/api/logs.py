686c1bdd9b7911a229526b297b17a9c7a5be8db5
zNitche <53448802+zNitche@users.noreply.github.com>
2024-11-14 21:57:04 +0100

    api log files related routes improvements

--- pico_weather_station/routes/api/logs.py
+++ pico_weather_station/routes/api/logs.py
@@ -10,16 +10,24 @@ logs = Router("logs", url_prefix="/api/logs")
 
 @logs.route("/")
 async def all_logs(request):
-    files = os.listdir(consts.LOGS_DIR_PATH)\
-        if files_utils.check_if_exists(consts.LOGS_DIR_PATH) else []
+    if not files_utils.check_if_exists(consts.LOGS_DIR_PATH):
+        return Response(payload=jsonify({"logs": []}))
 
-    files = [file.replace(".txt", "") for file in files]
+    sorted_files = files_utils.get_files_sorted_by_timestamp(path=consts.LOGS_DIR_PATH)
+    files = [file.split("_")[1].replace(".txt", "") for file in sorted_files]
 
     return Response(payload=jsonify({"logs": files}))
 
 
 @logs.route("/:date")
 async def log_content(request, date: str):
-    log_path = f"{consts.LOGS_DIR_PATH}/{date}.txt"
+    log_path = None
+
+    for file in os.listdir(consts.LOGS_DIR_PATH):
+        if file.endswith(f"{date}.txt"):
+            log_path = f"{consts.LOGS_DIR_PATH}/{file}"
+
+    if log_path is None:
+        return Response(status_code=404)
 
     return FileResponse(file_path=log_path)
